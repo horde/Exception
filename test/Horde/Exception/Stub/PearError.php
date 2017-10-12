8aac8584effa6dd82f05502617e3c9c12b66826e
Jan Schneider <jan@horde.org>
2017-10-12 17:02:58 +0200

    Remove incorrect links.

--- test/Horde/Exception/Stub/PearError.php
+++ test/Horde/Exception/Stub/PearError.php
@@ -8,7 +8,6 @@
  * @package  Exception
  * @author   Gunnar Wrobel <wrobel@pardus.de>
  * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
- * @link     http://pear.horde.org/index.php?package=Exception
  */
 class Horde_Exception_Stub_PearError
 {
