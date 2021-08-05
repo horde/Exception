542c7402329f5795ebd85dea12292f01632ffe94
Ralf Lang <lang@b1-systems.de>
2021-08-05 14:23:39 +0000

    Simplify Unit Testing (may break due to dependencies not PHP 8 ready)

---
+++ src/Translation.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2008-2021 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL-2.1
+ * @package  Exception
+ */
+namespace Horde\Exception;
+use Horde_Translation_Autodetect as TranslationAutodetect;
+/**
+ * Horde_Exception_Translation is the translation wrapper class for Horde_Exception.
+ *
+ * @author    Jan Schneider <jan@horde.org>
+ * @category  Horde
+ * @copyright 2010-2021 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL
+ * @package   Exception
+ */
+class Translation extends TranslationAutodetect
+{
+    /**
+     * The translation domain
+     *
+     * @var string
+     */
+    protected static $_domain = 'Horde_Exception';
+
+    /**
+     * The absolute PEAR path to the translations for the default gettext handler.
+     *
+     * @var string
+     */
+    protected static $_pearDirectory = '@data_dir@';
+}
